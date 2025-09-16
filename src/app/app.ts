50362f4242b6110ec68bee56cb98aa82f95d74de
Kiev Andres <kievmendoza30@gmail.com>
2025-09-16 15:35:19 -0400

    enable box icons for use with web components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
 import { ProgressCircle } from "./progress-circle/progress-circle";
 
 @Component({
   selector: 'app-root',
   imports: [ProgressCircle],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class App {
   protected readonly title = signal('pomodoro-chrome-extension');

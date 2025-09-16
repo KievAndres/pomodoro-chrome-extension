f83afc12fce90846df1b9e97ba491ec6211246b2
Kiev Andres <kievmendoza30@gmail.com>
2025-09-16 15:21:50 -0400

    add box icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ProgressCircle } from "./progress-circle/progress-circle";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProgressCircle],
+  imports: [ProgressCircle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

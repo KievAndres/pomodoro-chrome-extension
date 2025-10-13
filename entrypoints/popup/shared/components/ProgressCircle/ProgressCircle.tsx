798dc3a26267e751c33335a2811a7ab4d57e3ea2
Kiev Andres <kievmendoza30@gmail.com>
2025-10-13 18:14:27 -0400

    Add theme for break

--- entrypoints/popup/shared/components/ProgressCircle/ProgressCircle.tsx
+++ entrypoints/popup/shared/components/ProgressCircle/ProgressCircle.tsx
@@ -14,19 +14,28 @@ export default function ProgressCircle({
   const progress = Math.min((value / maxValue) * 100, 100);
   const strokeDashArray = `${circumference} ${circumference}`;
   const storkeDashOffset = circumference - (progress / 100) * circumference;
+  const STOP_COLOR_1_PREFIX = 'stop-color-1-theme';
+  const STOP_COLOR_2_PREFIX = 'stop-color-2-theme';
 
   const [stopColor1, setStopColor1] = useState<string>('#FC466B');
   const [stopColor2, setStopColor2] = useState<string>('#3F5EFB');
-
+  const [stopColor1ClassName, setStopColor1ClassName] = useState<string>(`${STOP_COLOR_1_PREFIX}-focus-1`);
+  const [stopColor2ClassName, setStopColor2ClassName] = useState<string>(`${STOP_COLOR_2_PREFIX}-focus-1`);
+  
   useEffect(() => {
     switch (theme) {
       case ProgressCircleTheme.Focus1:
+      default:
         setStopColor1('#FC466B');
         setStopColor2('#3F5EFB');
+        setStopColor1ClassName(`${STOP_COLOR_1_PREFIX}-focus-1`);
+        setStopColor2ClassName(`${STOP_COLOR_2_PREFIX}-focus-1`);
         break;
       case ProgressCircleTheme.Break1:
-        setStopColor1('#3F5EFB');
-        setStopColor2('#FC466B');
+        setStopColor1('#ecec49');
+        setStopColor2('#26e86a');
+        setStopColor1ClassName(`${STOP_COLOR_1_PREFIX}-break-1`);
+        setStopColor2ClassName(`${STOP_COLOR_2_PREFIX}-break-1`);
         break;
     }
   }, [theme]);
@@ -35,13 +44,13 @@ export default function ProgressCircle({
     <svg xmlns="https://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 40 40">
       <defs>
         <linearGradient id="gradient" x1="80%" y1="20%" x2="5%" y2="5%" spreadMethod="pad">
-          <stop offset="0%" stopColor={stopColor1} className="stop-color-1-theme-focus-1" />
-          <stop offset="100%" stopColor={stopColor2} className="stop-color-2-theme-focus-1" />
+          <stop offset="0%" stopColor={stopColor1} className={stopColor1ClassName} />
+          <stop offset="100%" stopColor={stopColor2} className={stopColor2ClassName} />
         </linearGradient>
 
         <linearGradient id="textGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor={stopColor1} className="stop-color-1-theme-focus-1" />
-          <stop offset="100%" stopColor={stopColor2} className="stop-color-2-theme-focus-1" />
+          <stop offset="0%" stopColor={stopColor1} className={stopColor1ClassName} />
+          <stop offset="100%" stopColor={stopColor2} className={stopColor2ClassName} />
         </linearGradient>
       </defs>
 

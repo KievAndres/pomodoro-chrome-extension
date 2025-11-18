3fd95c4398648eab997c122abaf5e8c1a3a2b132
Kiev Andres <kievmendoza30@gmail.com>
2025-11-18 09:52:27 -0400

    session completed test

--- public/sessionCompleted.js
+++ public/sessionCompleted.js
@@ -7,23 +7,22 @@
     }
 
     const POMODORO_STATE_KEY = 'pomodoroState';
-    const statusLabelMapper = {
-      ['FOCUS']: 'Focus',
-      ['SHORT_BREAK']: 'Short Break',
-      ['LONG_BREAK']: 'Long Break',
-      ['LONG_BREAK']: 'Idle',
-      ['LONG_BREAK']: 'Paused',
-    }
+    const TAB_NOTIFICATION_DATA_KEY = 'tabNotificationData';
 
-    const storageResult = await storage.get(POMODORO_STATE_KEY);
+    const storageResult = await storage.get([POMODORO_STATE_KEY, TAB_NOTIFICATION_DATA_KEY]);
     const pomodoroState = storageResult[POMODORO_STATE_KEY];
+    const tabNotificationData = storageResult[TAB_NOTIFICATION_DATA_KEY];
     if (!pomodoroState) {
       console.error('No pomodoro notification data found in storage');
       return;
     }
+    if (!tabNotificationData) {
+      console.error('No tab notification data found in storage');
+      return;
+    }
 
     const { status, nextStatus, focusCompleted, cyclesCompleted } = pomodoroState;
-    console.log({ status, nextStatus, focusCompleted, cyclesCompleted });
+    console.log({ pomodoroState, tabNotificationData });
   } catch (error) {
     console.error('Error reading from storage', error);
   }
